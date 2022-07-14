1c3763ace5ab488ff641bd34ea1e04c0da93f10c
ZeeshanTest <zeeshancs619@gmail.com>
2022-07-14 18:16:47 +0500

    Added Hook Folder

---
+++ src/Components/Hooks/EffectHook.js
@@ -0,0 +1,12 @@
+
+import react, { useEffect } from 'react';
+import axios from 'axios';
+let EffectHook = () => {
+    useEffect(()=>{
+        console.log("this is use effect");
+    });
+    return(
+
+    );
+}
+export default EffectHook;
